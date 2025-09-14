e14b575ef6e6c534ae1b52ce30c773f468fef5ef
fernanda-ihjaz <205657@upf.br>
2025-09-14 16:15:08 -0300

    fix: fix previous commit for menu implementation

--- index.cpp
+++ index.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include "include/functions.hpp"
 
 using namespace std;
 
 int main() {
+    int option = 0;
+    do {
+        cout << "\n- - - - - SIMULADOR DE GERENCIAMENTO DE MEMÓRIA - - - - -\n";
+        cout << "\nEscolha o modo de simulação:\n";
+        cout << "1. Alocação Contígua Dinâmica\n";
+        cout << "2. Paginação Pura\n";
+        cout << "3. Sair\n";
+        cout << "\nDigite sua opção: ";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                allocateContiguous();
+                break;
+            case 2:
+                // ALocação por paginação pura
+                break;
+            case 3:
+                cout << "Encerrando simulador...\n";
+                break;
+            default:
+                cout << "Opção inválida, tente novamente.\n";
+        }
+    } while (option != 3);
+
     return 0;
 }
