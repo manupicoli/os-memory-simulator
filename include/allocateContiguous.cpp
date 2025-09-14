ee32f4a2409a0bafb49506fc4ca9c984adf2399d
fernanda-ihjaz <205657@upf.br>
2025-09-14 16:06:13 -0300

    feat: add an include folder to centralize functions and a .hpp file to declare these functions

---
+++ include/allocateContiguous.cpp
@@ -0,0 +1,8 @@
+#include "functions.hpp"
+#include <iostream>
+
+using namespace std;
+
+void allocateContiguous() {
+    cout << "MODO: Alocação Contígua.\n";
+}
---
+++ include/functions.hpp
@@ -0,0 +1,7 @@
+#ifndef FUNCTIONS_HPP //ifndef é pra verificar se FUNCTIONS_HPP está definido
+#define FUNCTIONS_HPP //define, se não estava
+
+
+#include "allocateContiguous.cpp"
+
+#endif //finaliza o ifndef
