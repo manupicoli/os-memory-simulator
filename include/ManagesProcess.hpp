52c0c090a91bda959cf24da01e85566096349efd
fernanda-ihjaz <205657@upf.br>
2025-09-14 21:17:42 -0300

    feat: add process management function and header for domain encapsulation

---
+++ include/ManagesProcess.hpp
@@ -0,0 +1,6 @@
+#ifndef MANAGESPROCESS_HPP //ifndef é pra verificar se MANAGESPROCESS_HPP está definido
+#define MANAGESPROCESS_HPP //define, se não estava
+
+#include "managesProcess.cpp"
+
+#endif //finaliza o ifndef
